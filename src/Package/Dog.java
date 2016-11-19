41bee05de384e0a6cb00e62c8d71113966e8ecef
rylr <league@WTS9.attlocal.net>
2016-11-19 10:33:21 -0800

    finished
    i’m caught up

---
+++ src/Package/Dog.java
@@ -0,0 +1,24 @@
+package Package;
+public class Dog {
+	int numTeeth;
+	String name;
+	boolean hair;
+	
+	void nameDog(String name){
+		this.name = name;
+	}
+	void setTeeth(int numTeeth){
+		this.numTeeth = numTeeth;
+	}
+	void setHair(boolean hair){
+		this.hair = hair;
+	}
+	void talkAboutDog(){
+		System.out.println("Your dog's name is " + name + " it has " + numTeeth + " teeth, does it have hair: " + hair );
+	}
+	
+	public Dog(){
+		
+	}
+}
+
---
+++ src/Package/dogRunner.java
@@ -0,0 +1,12 @@
+package Package;
+
+public class dogRunner {
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		
+		d.setHair(true);
+		d.setTeeth(1);
+		d.nameDog("doggo");
+		d.talkAboutDog();
+	}
+}
